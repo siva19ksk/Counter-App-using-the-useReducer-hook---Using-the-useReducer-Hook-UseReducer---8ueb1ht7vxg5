661e41a86e7c8edba3ee17619769b3b5eb41ad01
siva19ksk <siva19ksk@gmail.com>
2023-12-09 15:33:17 +0530

    Yess

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,25 @@ import React from 'react';
 import React, { useReducer } from 'react';
 
 export default function Home() {
+  const initialTaskState ={count:0};
+  const [taskState,dispatch]=useReducer(reducer,initialTaskState);
+  function reducer(state,action){
+    switch(action.type){
+      case 'add':
+      return{count:state.count + 1};
+      case 'delete':
+      return{count:state.count - 1};
+      case 'deleteAll':
+      return{count:0};
+    }
+  }
   return (
     <div id='main'>
       <h2>Task Counter</h2>
-      {/*<button onClick={() => dispatch({type: 'add'})} id="addTaskBtn">Add a task</button>
+      <button onClick={() => dispatch({type: 'add'})} id="addTaskBtn">Add a task</button>
       <button onClick={() => dispatch({type: 'delete'})}  id="delTaskBtn">Delete a task</button>
       <button onClick={() => dispatch({type: 'deleteAll'})}  id="delAllTaskBtn">Delete all tasks</button>
-  <h3 className="counter">Number of Tasks : {taskState.count}</h3>*/}
-
-      {/*Update the above code to achieve the solution*/}
+      <h3 className="counter">Number of Tasks : {taskState.count}</h3>
     </div>
   );
 }
